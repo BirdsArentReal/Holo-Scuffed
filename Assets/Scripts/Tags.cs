4d333f5dd873ccfb9892d065b408c10a5c604264
BirdsArentReal <rsyj04@gmail.com>
2023-07-03 20:40:19 +0800

    commit die

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+
+public static class Tags
+{
+    public const string BULLET = "Bullet";
+    public const string ENEMY = "Enemy";
+    public const string OBSTACLE = "Obstacle";
+    public const string PLAYER = "Player";
+    public const string SHIELD = "Shield";
+    public const string SPAWN_MANAGER = "SpawnManager";
+    
+}
